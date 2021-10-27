c426ffb625baef99921ed381afa7dfec8db5f224
Prajwal <prajwal.zurunge12@gmail.com>
2021-10-27 17:47:59 +0530

    adding first name validation program

---
+++ Regex-Day20/Patterns.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Regex_Day20
+{
+    class Patterns
+    {
+        public static string REGEX_FIRST_NAME = "^[A-Z]{1}[A-Za-z]{2,}$";
+
+        public bool validateFirstName(string fisrtName)
+        {
+            Console.WriteLine("FirstName Validation is :");
+
+            return Regex.IsMatch(fisrtName, REGEX_FIRST_NAME);
+            
+        }
+    }
+}
--- Regex-Day20/Program.cs
+++ Regex-Day20/Program.cs
@@ -7,6 +7,9 @@ namespace Regex_Day20
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to User Registration Program");
+
+            Patterns patterns = new Patterns();
+            Console.WriteLine(patterns.validateFirstName("Prajwal"));
         }
     }
 }
